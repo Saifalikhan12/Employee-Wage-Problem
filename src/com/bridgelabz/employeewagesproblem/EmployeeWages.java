fe72f383f98354f672062ef5bb2aec61d036a46e
Saifalikhan12 <saifalikhan8147881@gmail.com>
2022-03-15 18:38:15 +0530

    MODIFIED: Check Employee Present or Absent

--- src/com/bridgelabz/employeewagesproblem/EmployeeWages.java
+++ src/com/bridgelabz/employeewagesproblem/EmployeeWages.java
@@ -1,9 +1,20 @@
 package com.bridgelabz.employeewagesproblem;
-
 public class EmployeeWages {
     public static void main(String[] args) {
 
         //Welcome Message for Initial Purpose
         System.out.println("Welcome to Employee Wages Problem Developed.");
+
+        //Constants
+        int IS_FULL_TIME = 1;
+
+        //Computation
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        if(empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
